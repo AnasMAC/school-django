e381c9f127b417d877f35e1d2bca1a87df1df4d4
akramelassri <168238434+akramelassri@users.noreply.github.com>
2026-05-19 12:30:50 +0100

    Add models

--- formations/models.py
+++ formations/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Formation(models.Model):
+    formation_name = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.IntegerField()  # in weeks or months
+
+    def __str__(self):
+        return self.formation_name
--- students/models.py
+++ students/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from formations.models import Formation
 
-# Create your models here.
+class StudentProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    formation = models.ForeignKey(
+        Formation,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
